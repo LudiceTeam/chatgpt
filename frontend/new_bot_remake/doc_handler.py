95b85e51a19fd8e71fccdb78d080c759983b4192
Wizer27 <svd2yar@gmail.com>
2026-01-24 17:26:19 +0300

    read doc

---
+++ frontend/new_bot_remake/doc_handler.py
@@ -0,0 +1,19 @@
+from docx import Document
+import asyncio
+
+
+async def extract(path:str) -> str:
+    try:
+        doc = Document(path)
+        text_pr = []
+        for pr in doc.paragraphs:
+            if pr.text.strip():
+                text_pr.append(pr.text)
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    if cell.text.strip():
+                        text_pr.append(cell.text)  
+        return "\n\n".join(text_pr)                      
+    except Exception as e:
+        raise Exception(f"Error : {e}")
--- frontend/new_bot_remake/handlers.py
+++ frontend/new_bot_remake/handlers.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import fitz
 from pdf2image import convert_from_path
 import zipfile
+from docx import Document
 
 
 
@@ -182,7 +183,20 @@ async def read_pdf(path:str) -> str:
         raise Exception(f"Error : {e}")            
     
 async def read_doc(path:str) -> str:
-    pass
+    try:
+        doc = Document(path)
+        text_pr = []
+        for pr in doc.paragraphs:
+            if pr.text.strip():
+                text_pr.append(pr.text)
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    if cell.text.strip():
+                        text_pr.append(cell.text)  
+        return "\n\n".join(text_pr)                      
+    except Exception as e:
+        raise Exception(f"Error : {e}")
 
 
 @router.message(F.document)
